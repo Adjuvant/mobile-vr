c76ce58a429343f1aad73b3a21a822a469df79c4
adjuvant <tom@layeraudio.com>
2017-11-08 13:32:24 +0000

    Separate examples to inert and interactive plinths

---
+++ Assets/01_Make a VR Experience/Scripts/TimedObjectChanger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedObjectChanger : MonoBehaviour {
+
+	[SerializeField]
+	float objectChangeTime = 5f;
+	private IEnumerator coroutine;
+
+	// Use this for initialization
+	void Start () {
+		// Start function WaitAndError as a coroutine.
+		coroutine = WaitAndChange(objectChangeTime);
+		StartCoroutine(coroutine);
+	}
+
+	private IEnumerator WaitAndChange(float waitTime)
+	{		
+		while (true) {
+			yield return new WaitForSeconds (waitTime);
+			if(gameObject.GetComponent<ObjectChanger> ())
+				gameObject.GetComponent<ObjectChanger> ().ChangeObject ();
+		}
+	}
+}
